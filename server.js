032a7669023361a46d95ac7c6c42b916f8296bed
otavioogo <otavioeidi@hotmail.com>
2024-11-25 17:16:44 -0300

    GET , PUT , POST e DELETE  ( USER)

--- server.js
+++ server.js
@@ -1,21 +1,73 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = [] //salvando usuarios dentro dessa variavel
 
-app.post('/usuarios', (req , res) => {  // criando os usuarios com metodo post json
-    users.push(req.body)
-    res.send("deu bom !")
+app.post('/usuarios', async (req , res) => {  // criando os usuarios com metodo post json
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age
+            }
+        })
+    } else {
+        users = await prisma.user.findMany()
+    }
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name,
+        }
+    })
+
+    res.status(201).json(req.body)
 
 })
 
-app.get('/usuarios', (req, res ) => {  // listando os usuarios metodo get
-    res.json(users)
+app.get('/usuarios', async (req, res ) => {  // listando os usuarios metodo get
+    
+    const users = await prisma.user.findMany()
+
+        res.status(200).json(users)
     }   
 )
+
+app.put('/usuarios/:id', async (req , res) => {  // criando os usuarios com metodo post json
+
+    await prisma.user.update({
+        where: { id: req.params.id },
+
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name,
+        }
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+
+app.delete('/usuarios/:id', async (req , res) => {  // criando os usuarios com metodo post json
+
+    await prisma.user.delete({
+        where: { id: req.params.id }
+    })
+
+    res.status(200).json({ message: 'Usuario Deletado '})
+
+})
+
 
 app.listen(3000)
 
