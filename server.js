bc0e52a7d6a1d466fbc04acb9eb2abab7505a8ba
otavioogo <otavioeidi@hotmail.com>
2024-11-25 15:39:54 -0300

    Criando validacao back

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-SERVER . js
+import express from 'express'
+
+
+const app = express()
+app.use(express.json())
+
+const users = [] //salvando usuarios dentro dessa variavel
+
+app.post('/usuarios', (req , res) => {  // criando os usuarios com metodo post json
+    users.push(req.body)
+    res.send("deu bom !")
+
+})
+
+app.get('/usuarios', (req, res ) => {  // listando os usuarios metodo get
+    res.json(users)
+    }   
+)
+
+app.listen(3000)
+
+
